e2c4a46bde8ff4a1b6d2c50501775684d361b120
Engel <andrea.engel@sap.com>
2020-04-07 18:32:48 +0200

    Handlebars added

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 //create a simple express server
 const express = require('express');
-const app = express();
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const products = require('./Products')
+
+const app = express();
 
 //init middleware
 //app.use(logger);
-//BodyParser middleware
-app.use(express.json());                        //handle raw json
-app.use(express.urlencoded({extended:false}));  //handle formas  
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));     //emtplate engine set to handlebars; default layout = file with the name "main"
+app.set('view engine', 'handlebars');                          //set voiew engine   
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//homepage route
+app.get('/', (req, res) => res.render('index', {
+    title: "Shopping Assist",
+    products
+}));
 
 //set static folder
 //point to the folder that is going to be used as static folder
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 //Products API Routes
 app.use('/api/products', require('./routes/api/products'));
